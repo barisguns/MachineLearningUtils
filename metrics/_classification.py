0f4c0c7b699978f4f44a9a6c2863493e9ae5b44d
Barış Gün Sürmeli <barisgunsurmeli@gmail.com>
2023-06-08 15:13:41 +0200

    - Added a metrics directory to contain evaluation metrics related source. - Added custom sklearn scorers that would return confusion matrix as a whole or each of the components separately.

---
+++ metrics/_classification.py
@@ -0,0 +1,73 @@
+from sklearn.metrics import confusion_matrix
+
+
+def confusion_matrix_scorer(clf, X, y):
+    """
+    This custom scorer follows sklearn scorer signature and takes classifier, data matrix and true labels as input.
+    It returns a dictionary that includes confusion matrix components which are accessed by
+     calling sklearn.metrics.confusion_matrix().
+    :param clf: A sklearn classifier object.
+    :param X: Data matrix.
+    :param y: True labels.
+    :return:
+    """
+    y_pred = clf.predict(X)
+    cm = confusion_matrix(y, y_pred)
+    return {'tn': cm[0, 0], 'fp': cm[0, 1],
+            'fn': cm[1, 0], 'tp': cm[1, 1]}
+
+
+def true_positive_scorer(clf, X, y):
+    """
+    This custom scorer follows sklearn scorer signature and takes classifier, data matrix and true labels as input.
+    It returns the number of true positives which is accessed by calling sklearn.metrics.confusion_matrix().
+    :param clf:
+    :param X:
+    :param y:
+    :return:
+    """
+    y_pred = clf.predict(X)
+    cm = confusion_matrix(y, y_pred)
+    return cm[1, 1]
+
+
+def true_negative_scorer(clf, X, y):
+    """
+    This custom scorer follows sklearn scorer signature and takes classifier, data matrix and true labels as input.
+    It returns the number of true negatives which is accessed by calling sklearn.metrics.confusion_matrix().
+    :param clf:
+    :param X:
+    :param y:
+    :return:
+    """
+    y_pred = clf.predict(X)
+    cm = confusion_matrix(y, y_pred)
+    return cm[0, 0]
+
+
+def false_negative_scorer(clf, X, y):
+    """
+    This custom scorer follows sklearn scorer signature and takes classifier, data matrix and true labels as input.
+    It returns the number of false negatives which is accessed by calling sklearn.metrics.confusion_matrix().
+    :param clf:
+    :param X:
+    :param y:
+    :return:
+    """
+    y_pred = clf.predict(X)
+    cm = confusion_matrix(y, y_pred)
+    return cm[1, 0]
+
+
+def false_positive_scorer(clf, X, y):
+    """
+    This custom scorer follows sklearn scorer signature and takes classifier, data matrix and true labels as input.
+    It returns the number of false positives which is accessed by calling sklearn.metrics.confusion_matrix().
+    :param clf:
+    :param X:
+    :param y:
+    :return:
+    """
+    y_pred = clf.predict(X)
+    cm = confusion_matrix(y, y_pred)
+    return cm[0, 1]
